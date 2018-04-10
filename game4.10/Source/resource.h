35d7ef2ba280f345d1fa07c4b40f3f555599e763
LAPTOP-DEQT4DRO\Chou Yu Tain <hannis505@gmail.com>
2018-04-10 22:05:39 +0800

    地圖改版

--- game4.10/Source/resource.h
+++ game4.10/Source/resource.h
@@ -123,8 +123,9 @@
 #define IDB_TITLE_16                    243
 #define IDB_TITLE_17                    244
 #define IDB_TITLE_18                    245
-#define IDB_BITMAP26                    246
 #define IDB_TITLE_19                    246
+#define IDB_BITMAP6                     248
+#define IDB_BACKGROUND                  248
 #define IDC_README                      1001
 #define ID_FILE_PAUSE                   32771
 #define ID_TOGGLE_FULLSCREEN            32772
@@ -137,7 +138,7 @@
 #ifdef APSTUDIO_INVOKED
 #ifndef APSTUDIO_READONLY_SYMBOLS
 #define _APS_3D_CONTROLS                     1
-#define _APS_NEXT_RESOURCE_VALUE        247
+#define _APS_NEXT_RESOURCE_VALUE        249
 #define _APS_NEXT_COMMAND_VALUE         32776
 #define _APS_NEXT_CONTROL_VALUE         1003
 #define _APS_NEXT_SYMED_VALUE           101
