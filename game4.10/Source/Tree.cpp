3eabaf5d01797f4c173d041083995ff84be21409
a105590015 <t105590015@ntut.org.tw>
2018-05-18 04:55:56 +0800

    石頭

--- game4.10/Source/Tree.cpp
+++ game4.10/Source/Tree.cpp
@@ -27,8 +27,8 @@ namespace game_framework {
 		die.AddBitmap(".\\RES\\King\\King_die_1.bmp", RGB(255, 255, 255));
 		die.AddBitmap(".\\RES\\King\\King_die_2.bmp", RGB(255, 255, 255));
 		die.AddBitmap(".\\RES\\King\\King_die_3.bmp", RGB(255, 255, 255));
-
-
+		for (int i = 0;i < 12;i++)
+			stone[i].LoadBitmap();
 	}
 
 	void Tree::Initialize(int px,int py) {
@@ -43,13 +43,25 @@ namespace game_framework {
 		RightOrLeft = true;
 
 		index = &normal;
+		stone[0].Init(0, 0);
+		stone[1].Init(54, 0);
+		stone[2].Init(108, 0);
+		stone[3].Init(162, 0);
+		stone[4].Init(216, 0);
+		stone[5].Init(270, 0);
+		stone[6].Init(324, 0);
+		stone[7].Init(378, 0);
+		stone[8].Init(432, 0);
+		stone[9].Init(486, 0);
+		stone[10].Init(540, 0);
+		stone[11].Init(594, 0);
 	}
 
 	void Tree::OnMove(Map* m,Kirby* k) {
 
 		if (is_alive) {
 			counter++;
-
+			
 		}
 		else
 		{
@@ -63,9 +75,20 @@ namespace game_framework {
 		index->SetTopLeft(m->ScreenX(x), m->ScreenY(y));
 		index->SetDelayCount(16);
 		index->OnShow();
+		if(is_alive)
+			Attack(m, k);
 	}
 
 	void Tree::Attack(Map* m, Kirby* k) {
+		int r = rand() % 360;
+		for (int i = 0;i < 12;i++)
+		{
+			if (r == i)
+				stone[i].SetAlive(true);
+
+			stone[i].OnMove(m,k);
+			stone[i].OnShow(m,k);
+		}
 
 	}
 
